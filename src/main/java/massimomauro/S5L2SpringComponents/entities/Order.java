2706fc635ae804d8e72ad8a6b3022c4aca488234
MrMax01 <massimo.mauro00@gmail.com>
2023-10-31 16:56:35 +0100

    end exercize

--- src/main/java/massimomauro/S5L2SpringComponents/entities/Order.java
+++ src/main/java/massimomauro/S5L2SpringComponents/entities/Order.java
@@ -1,11 +1,15 @@
 package massimomauro.S5L2SpringComponents.entities;
 
 import massimomauro.S5L2SpringComponents.Enums.OrderStatus;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 
 import java.time.LocalTime;
 import java.util.List;
 
+@PropertySource("application.properties")
 public class Order {
+
     private long id;
     private Table table;
     private List<MenuElement> orderList;
@@ -15,6 +19,9 @@ public class Order {
     private int personNum;
     private double totalCost;
 
+    @Value("$costo.coperto}")
+    private double coverCharge;
+
     public Order(long id, Table table, List<MenuElement> orderList, OrderStatus status, LocalTime time, int personNum) {
         this.id = id;
         this.table = table;
@@ -22,10 +29,12 @@ public class Order {
         this.status = status;
         this.time = time;
         this.personNum = personNum;
+        setTotalCost();
 
     }
 
     public void setTotalCost() {
-        this.totalCost= this.orderList.stream().mapToDouble(MenuElement::getPrice).sum();
+
+        this.totalCost = this.orderList.stream().mapToDouble(MenuElement::getPrice).sum()+(this.coverCharge*this.personNum);
     }
 }
